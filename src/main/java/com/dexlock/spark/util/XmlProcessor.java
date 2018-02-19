72defcd38727d1eea78b3416ed553947a8266ed7
mrudul <mrudul@dexlock.com>
2018-02-19 19:47:11 +0530

    done loading of xml to table

--- src/main/java/com/dexlock/spark/util/XmlProcessor.java
+++ src/main/java/com/dexlock/spark/util/XmlProcessor.java
@@ -1,8 +1,16 @@
 package com.dexlock.spark.util;
 
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+import scala.xml.XML$;
+
 import java.io.File;
 
 public class XmlProcessor {
@@ -23,18 +31,32 @@ public class XmlProcessor {
     }
 
     public static void processWikiDump(String filePath, SparkSession sparkSession){
-       /* Dataset<Row> wikiDataSet = sparkSession.sqlContext().read()
+        Dataset<Row> wikiDataSet1 = sparkSession.sqlContext().read()
                 .format("com.databricks.spark.xml")
-                //.option("rowTag", "page")
-                .option("valueTag", "page")
+                .option("rowTag", "pages")
                 .load(filePath);
-        wikiDataSet.show(30);*/
-
-       sparkSession.sql("CREATE TABLE wikidata (_id string, _title string, _revision string, _type string, page string)\n" +
+        //wikiDataSet1.show(30);
+       // System.out.println("****\n");
+       // wikiDataSet1.printSchema();
+        JavaRDD expandedWikiRDD = wikiDataSet1.javaRDD().map(row->{
+            return row.get(0);
+        });
+        System.out.println(expandedWikiRDD.top(1));
+        StructType customSchema = new StructType(new StructField[] {
+                new StructField("_VALUE", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("_id", DataTypes.LongType, true, Metadata.empty()),
+                new StructField("_ns-id", DataTypes.LongType, true, Metadata.empty()),
+                new StructField("_ns-name", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("_revision", DataTypes.LongType, true, Metadata.empty()),
+                new StructField("_title", DataTypes.StringType, true, Metadata.empty()),
+                new StructField("_type", DataTypes.StringType, true, Metadata.empty())
+        });
+  /*     sparkSession.sql("CREATE TABLE wikidata (_id string, _title string, _revision string, _type string, _page string, _nsid string, _nsname string, description string)\n" +
                "USING com.databricks.spark.xml\n" +
-               "OPTIONS (path \""+filePath+"\", rootTag \"page\")");
+               "OPTIONS (path \""+filePath+"\", rowTag \"pages\", valueTag \"page\")");
        Dataset<Row> wikiDataSet = sparkSession.sql("SELECT * FROM wikidata limit 100");
-       wikiDataSet.show(30);
+       wikiDataSet.show(30);*/
+
     }
 
 }
